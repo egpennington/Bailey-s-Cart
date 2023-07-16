ae5e59865ed75e7fe72524f82d23a862bb9cc66a
egpennington <53251137+egpennington@users.noreply.github.com>
2023-07-16 12:25:25 -0500

    Update main.js
    added category databases to organize shopping list better

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* items set up for firebase usage */
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
@@ -7,59 +8,272 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
 
+/* Catagories of shopping items */
+const produceListInDB = ref(database, "produce")
+const dairyListInDb = ref(database, "dairy")
+const dryListInDb = ref(database, "dry")
+const frozenListInDb = ref(database, "frozen")
+const miscListInDb = ref(database, "misc")
+const medicineListInDb = ref(database, "medicine")
+
+/* input and buttons */
 const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
-const shoppingListEl = document.getElementById("shopping-list")
+const addButtonProduceEl = document.getElementById("add-button-produce")
+const addButtonDairyEl = document.getElementById("add-button-dairy")
+const addButtonDryEl = document.getElementById("add-button-dry")
+const addButtonFrozenEl = document.getElementById("add-button-frozen")
+const addButtonMiscEl = document.getElementById("add-button-misc")
+const addButtonMedicineEl = document.getElementById("add-button-medicine")
 
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
-    push(shoppingListInDB, inputValue)
-    
+/* list display areas */
+const produceListEl = document.getElementById("produce-list-el")
+const dairyListEl = document.getElementById("dairy-list-el")
+const dryListEl = document.getElementById("dry-list-el")
+const frozenListEl = document.getElementById("frozen-list-el")
+const miscListEl = document.getElementById("misc-list-el")
+const medicineListEl = document.getElementById("medicine-list-el")
+
+
+/* catagory button inputs to list areas*/
+addButtonProduceEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value    
+    push(produceListInDB, inputValue)    
     clearInputFieldEl()
 })
 
-onValue(shoppingListInDB, function(snapshot) {
+addButtonDairyEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value    
+    push(dairyListInDb, inputValue)    
+    clearInputFieldEl()
+})
+
+addButtonDryEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    push(dryListInDb, inputValue)    
+    clearInputFieldEl()
+})
+
+addButtonFrozenEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    push(frozenListInDb, inputValue)    
+    clearInputFieldEl()
+})
+
+addButtonMiscEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    push(miscListInDb, inputValue)    
+    clearInputFieldEl()
+})
+
+addButtonMedicineEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    push(medicineListInDb, inputValue)    
+    clearInputFieldEl()
+})
+
+/* database snapshots for each catagory */
+onValue(produceListInDB, function(snapshot) {
     if (snapshot.exists()) {
         let itemsArray = Object.entries(snapshot.val())
     
-        clearShoppingListEl()
+        clearProduceListEl()
         
         for (let i = 0; i < itemsArray.length; i++) {
             let currentItem = itemsArray[i]
             let currentItemID = currentItem[0]
             let currentItemValue = currentItem[1]
             
-            appendItemToShoppingListEl(currentItem)
+            appendItemToProduceListEl(currentItem)
         }    
     } else {
-        shoppingListEl.innerHTML = "No items here... yet"
+        produceListEl.innerHTML = "No items here... yet"
     }    
 })
 
-function clearShoppingListEl() {
-    shoppingListEl.innerHTML = ""
+onValue(dairyListInDb, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearDairyListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToDairyListEl(currentItem)
+        }    
+    } else {
+        dairyListEl.innerHTML = "No items here... yet"
+    }    
+})
+
+onValue(dryListInDb, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearDryListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToDryListEl(currentItem)
+        }    
+    } else {
+        dryListEl.innerHTML = "No items here... yet"
+    }    
+})
+
+onValue(frozenListInDb, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearFrozenListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToFrozenListEl(currentItem)
+        }    
+    } else {
+        frozenListEl.innerHTML = "No items here... yet"
+    }    
+})
+
+onValue(miscListInDb, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearMiscListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToMiscListEl(currentItem)
+        }    
+    } else {
+        miscListEl.innerHTML = "No items here... yet"
+    }    
+})
+
+onValue(medicineListInDb, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearMedicineListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToMedicineListEl(currentItem)
+        }    
+    } else {
+        medicineListEl.innerHTML = "No items here... yet"
+    }    
+})
+
+/* clear lists functions */
+function clearProduceListEl() {
+    produceListEl.innerHTML = ""
+}
+function clearDairyListEl() {
+    dairyListEl.innerHTML = ""
+}
+function clearDryListEl() {
+    dryListEl.innerHTML = ""
+}
+function clearFrozenListEl() {
+    frozenListEl.innerHTML = ""
+}
+function clearMiscListEl() {
+    miscListEl.innerHTML = ""
+}
+function clearMedicineListEl() {
+    medicineListEl.innerHTML = ""
 }
 
 function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
 
-function appendItemToShoppingListEl(item) {
+/* append items database */
+function appendItemToProduceListEl(item) {
     let itemID = item[0]
-    let itemValue = item[1]
-    
-    let newEl = document.createElement("li")
-    
-    newEl.textContent = itemValue
-    
+    let itemValue = item[1]    
+    let newEl = document.createElement("li")    
+    newEl.textContent = itemValue    
     newEl.addEventListener("click", function() {
-        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
-        
+        let exactLocationOfItemInDB = ref(database, `produce/${itemID}`) 
         remove(exactLocationOfItemInDB)
-    })
-    
-    shoppingListEl.append(newEl)
+    })    
+    produceListEl.append(newEl)
+}
+
+function appendItemToDairyListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]    
+    let newEl = document.createElement("li")    
+    newEl.textContent = itemValue    
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `dairy/${itemID}`)   
+        remove(exactLocationOfItemInDB)
+    })    
+    dairyListEl.append(newEl)
+}
+
+function appendItemToDryListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]    
+    let newEl = document.createElement("li")    
+    newEl.textContent = itemValue    
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `dry/${itemID}`) 
+        remove(exactLocationOfItemInDB)
+    })    
+    dryListEl.append(newEl)
+}
+
+function appendItemToFrozenListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]    
+    let newEl = document.createElement("li")    
+    newEl.textContent = itemValue    
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `frozen/${itemID}`) 
+        remove(exactLocationOfItemInDB)
+    })    
+    frozenListEl.append(newEl)
+}
+
+function appendItemToMiscListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]    
+    let newEl = document.createElement("li")    
+    newEl.textContent = itemValue    
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `misc/${itemID}`) 
+        remove(exactLocationOfItemInDB)
+    })    
+    miscListEl.append(newEl)
+}
+
+function appendItemToMedicineListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]    
+    let newEl = document.createElement("li")    
+    newEl.textContent = itemValue    
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `medicine/${itemID}`) 
+        remove(exactLocationOfItemInDB)
+    })    
+    medicineListEl.append(newEl)
 }
